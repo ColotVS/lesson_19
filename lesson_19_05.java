910492beb8741f24494e6ef8ec86e4a1f6fdf6f0
Vladislav Kolotilov <kolotilov98@mail.ru>
2024-03-12 12:48:12 +0700

    Сортировка строк по длине в коллекциях Урок 19, задача 5

---
+++ lesson_19_05.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class lesson_19_05 {
+    //Полный код программы, которая сортирует строки по длине, будет выглядеть вот так:
+    public static void main(String[] args)
+    {
+        ArrayList<String> list = new ArrayList<String>();
+        Collections.addAll(list, "Привет", "как", "дела?");
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
+        for (String element:list){
+            System.out.println(element);
+        }
+    }
+}
+
+
+
